f1fc9683d08c17d269014f7f748962664ad6a152
klado <klaudiusz.pawezka@elev.ntig.se>
2021-01-19 16:07:05 +0100

    Start of generating random grid
    Co-Authored-By: Erik Reider <35975961+ErikReider@users.noreply.github.com>
    Co-Authored-By: Gustav <54705962+Gustavsahlberg@users.noreply.github.com>
    Co-Authored-By: DanTGL <33489389+DanTGL@users.noreply.github.com>
    Co-Authored-By: magnuslif <70268658+magnuslif@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:firebase_database/firebase_database.dart";
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart" show rootBundle;
 
 void main() {
   runApp(MyApp());
@@ -26,6 +27,20 @@ class MyHomePage extends StatefulWidget {
   MyHomePageState createState() => MyHomePageState();
 }
 
+class Word {
+  Word(this.word, this.grid, List<int> positions, List<int> directions) {
+    this.positions = List<int>.from(positions);
+    this.positions.shuffle();
+    this.directions = List<int>.from(directions);
+    this.directions.shuffle();
+  }
+
+  String word;
+  List<int> positions;
+  List<int> directions;
+  List<Char> grid;
+}
+
 class MyHomePageState extends State<MyHomePage> {
   bool loadedWords = false;
 
@@ -36,8 +51,105 @@ class MyHomePageState extends State<MyHomePage> {
 
   List<GlobalKey<TileState>> listOfKeys;
 
+  // Gets words from Firebase
   Future<dynamic> getWords() async =>
       (await FirebaseDatabase.instance.reference().child("Levels").child("1").once()).value;
+
+  List<Char> tryWord(List<Char> grid, String word, int gridSize, int position, int dir) {
+    final int xPos = position % gridSize;
+    final int yPos = (position / gridSize).floor();
+    switch (dir) {
+      case 0:
+        // Horizontal
+        if (xPos > gridSize - word.length) return null;
+        int index = xPos;
+        for (final String char in word.split("").toList()) {
+          final int i = index + (gridSize * yPos);
+          if (grid[i] != null || grid[i]?.char != char) return null;
+          grid[i] = Char(i, char);
+          index++;
+        }
+        break;
+      case 1:
+        // Vertical
+        if (yPos > gridSize - word.length) return null;
+        int index = yPos;
+        for (final String char in word.split("").toList()) {
+          final int i = xPos + (gridSize * index);
+          if (grid[i] != null || grid[i]?.char != char) return null;
+          grid[i] = Char(i, char);
+          index++;
+        }
+        break;
+      case 2:
+        // Diagonally
+        if (yPos > gridSize - word.length || xPos > gridSize - word.length) return null;
+        int index = yPos;
+        int diag = 0;
+        for (final String char in word.split("").toList()) {
+          final int i = xPos + (gridSize * index) + diag;
+          if (grid[i] != null || grid[i]?.char != char) return null;
+          grid[i] = Char(i, char);
+          index++;
+          diag++;
+        }
+        break;
+      default:
+    }
+    return grid;
+  }
+
+  List<Char> generateGrid(List<String> words, int gridSize) {
+    words.shuffle();
+    final List<int> positions = List<int>.generate(gridSize * gridSize, (int index) => index);
+    final List<int> directions = List<int>.generate(3, (int index) => index);
+
+    final List<Word> wordStack = <Word>[];
+
+    int wordIndex = 0;
+
+    wordStack.add(Word(words[wordIndex], List<Char>(gridSize * gridSize), positions, directions));
+
+    while (wordIndex < words.length) {
+      if (wordStack.isEmpty) {
+        throw Exception("Word does not fit on the grid");
+      }
+
+      if (wordStack.last.directions.isEmpty) {
+        wordStack.last.positions.removeLast();
+        wordStack.last.directions = List<int>.from(directions);
+        wordStack.last.directions.shuffle();
+      }
+
+      final int dir = wordStack.last.directions.removeLast();
+
+      if (wordStack.last.positions.isEmpty) {
+        wordIndex--;
+        wordStack.removeLast();
+      } else {
+        final int pos = wordStack.last.positions.last;
+        final List<Char> grid = tryWord(wordStack.last.grid, wordStack.last.word, gridSize, pos, dir);
+        if (grid != null) {
+          if (wordIndex < words.length - 1) {
+            wordIndex++;
+            wordStack.add(Word(words[wordIndex], grid, positions, directions));
+          } else {
+            break;
+          }
+        }
+      }
+    }
+
+    /*for (int i = 0; i < grid.length; i++) {
+      if (wordStack.grid[i] == null) {
+        
+      }
+    }*/
+
+    print(wordStack.last.grid);
+
+    return wordStack.last.grid;
+  }
 
   // Clears all tiles
   void clear() {
@@ -150,62 +262,78 @@ class MyHomePageState extends State<MyHomePage> {
                   future: getWords(),
                   builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
                     if (!snapshot.hasData) return Container();
-                    loadedWords = true;
-                    // Gets the column and row child count
-                    final int rowSize = int.parse(snapshot.data["gridSize"]?.toString());
-                    listOfKeys =
-                        List<GlobalKey<TileState>>.generate(rowSize * rowSize, (int i) => GlobalKey<TileState>());
+                    return FutureBuilder<dynamic>(
+                      future: rootBundle.loadString("assets/words.txt"),
+                      builder: (BuildContext context, AsyncSnapshot<dynamic> wordsSnapshot) {
+                        if (!wordsSnapshot.hasData) return Container();
+                        loadedWords = true;
+                        // Gets the column and row child count
+                        final int rowSize = int.parse(snapshot.data["gridSize"]?.toString());
+                        listOfKeys =
+                            List<GlobalKey<TileState>>.generate(rowSize * rowSize, (int i) => GlobalKey<TileState>());
 
-                    originalWords =
-                        (snapshot.data["correctWords"] as List<dynamic>).map((dynamic e) => e.toString()).toList();
-                    correctWords = originalWords;
+                        originalWords =
+                            (snapshot.data["correctWords"] as List<dynamic>).map((dynamic e) => e.toString()).toList();
+                        correctWords = originalWords;
 
-                    // [["M", "A", "G"], ["N", "U", "S"], ["M", "O", "R"]]
-                    final List<String> allWords = snapshot.data["letters"].toString().split("");
-                    final List<List<Char>> words = List<List<Char>>.generate(rowSize, (int i) {
-                      int index = 0;
-                      return allWords
-                          .getRange(i * rowSize, (i * rowSize) + rowSize)
-                          .toList()
-                          .map((String e) => Char(index++ + (i * rowSize), e))
-                          .toList();
-                    });
-                    return GridView.count(
-                      childAspectRatio: 1,
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      padding: const EdgeInsets.all(2),
-                      crossAxisCount: rowSize,
-                      children: List<Widget>.generate(
-                        rowSize * rowSize,
-                        (int index) => Tile(
-                          key: listOfKeys[index],
-                          char: words[(index / rowSize).floor()][index % rowSize],
-                          onClick: (Char char, bool selected) async {
-                            if (!selected) {
-                              usedLetters.add(char.id);
-                              usedLetters.sort();
-                              final int correctWordsLen = correctWords.length;
-                              if (hasWon()) {
-                                winnerWinner();
-                                clear();
-                              } else {
-                                final DatabaseReference lossesRef =
-                                    FirebaseDatabase.instance.reference().child("AmountOfLosses");
-                                lossesRef.once().then((DataSnapshot value) =>
-                                    lossesRef.set((int.parse(value.value.toString()) ?? 0) + 1));
-                                if (correctWordsLen != correctWords.length || usedLetters.length >= rowSize) clear();
-                              }
-                              final DatabaseReference gamesRef =
-                                  FirebaseDatabase.instance.reference().child("AmountOfGames");
-                              gamesRef.once().then(
-                                  (DataSnapshot value) => gamesRef.set((int.parse(value.value.toString()) ?? 0) + 1));
-                            } else {
-                              usedLetters.remove(char.id);
-                            }
-                          },
-                        ),
-                      ),
+                        final List<String> data = (wordsSnapshot.data as String).split("\n");
+                        final List<String> compatibleWords =
+                            data.where((String w) => w.length >= 2 && w.length <= rowSize).toList();
+                        compatibleWords.shuffle();
+                        final List<String> words2 = compatibleWords.getRange(0, 4).toList();
+                        print(words2);
+
+                        final List<Char> grid = generateGrid(words2, rowSize);
+                        print(grid);
+
+                        final List<String> allWords = snapshot.data["letters"].toString().split("");
+                        final List<List<Char>> words = List<List<Char>>.generate(rowSize, (int i) {
+                          int index = 0;
+                          return allWords
+                              .getRange(i * rowSize, (i * rowSize) + rowSize)
+                              .toList()
+                              .map((String e) => Char(index++ + (i * rowSize), e))
+                              .toList();
+                        });
+                        return GridView.count(
+                          childAspectRatio: 1,
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          padding: const EdgeInsets.all(2),
+                          crossAxisCount: rowSize,
+                          children: List<Widget>.generate(
+                            rowSize * rowSize,
+                            (int index) => Tile(
+                              key: listOfKeys[index],
+                              char: words[(index / rowSize).floor()][index % rowSize],
+                              onClick: (Char char, bool selected) async {
+                                if (!selected) {
+                                  usedLetters.add(char.id);
+                                  usedLetters.sort();
+                                  final int correctWordsLen = correctWords.length;
+                                  if (hasWon()) {
+                                    winnerWinner();
+                                    clear();
+                                  } else {
+                                    final DatabaseReference lossesRef =
+                                        FirebaseDatabase.instance.reference().child("AmountOfLosses");
+                                    lossesRef.once().then((DataSnapshot value) =>
+                                        lossesRef.set((int.parse(value.value.toString()) ?? 0) + 1));
+                                    if (correctWordsLen != correctWords.length || usedLetters.length >= rowSize)
+                                      clear();
+                                  }
+                                  final DatabaseReference gamesRef =
+                                      FirebaseDatabase.instance.reference().child("AmountOfGames");
+                                  gamesRef.once().then((DataSnapshot value) =>
+                                      gamesRef.set((int.parse(value.value.toString()) ?? 0) + 1));
+                                } else {
+                                  usedLetters.remove(char.id);
+                                }
+                              },
+                            ),
+                          ),
+                        );
+                      },
                     );
                   },
                 ),
@@ -260,5 +388,6 @@ class TileState extends State<Tile> {
 class Char {
   const Char(this.id, this.char);
   final int id;
+
   final String char;
 }
